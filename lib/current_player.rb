817cd5345eb97cb9a4262417dee285729025ec61
Matt Vierling <matt.vierling@wework.com>
2018-03-29 03:32:49 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -7,4 +7,10 @@ def turn_count(board)
   end
 end
 
-def current_player
+def current_player(board)
+  if turn_count(board).even?
+    puts "X"
+  elsif turn_count(board).odd?
+    puts "O"
+  end
+end
